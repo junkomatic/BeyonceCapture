e35a8908e5f43c3bb9e7f0d5b12b14f624e8c557
Mike Holberger <mholberger@gmail.com>
2018-02-03 01:27:56 -0700

    Data class progress

--- BeyonceCapture/BeyonceDATA.cs
+++ BeyonceCapture/BeyonceDATA.cs
@@ -13,13 +13,38 @@ namespace BeyonceCapture
 {
     public static class BeyonceDATA
     {
-        public static ConcurrentQueue<string> UpdateQueue = new ConcurrentQueue<string>();        
+        public static ConcurrentQueue<string> UpdateQueue = new ConcurrentQueue<string>();
         private static readonly MongoClient client = new MongoClient("mongodb://localhost:27017");
         private static readonly IMongoDatabase db = client.GetDatabase("marketData");
-        private static readonly IMongoCollection<BsonDocument> BTCdeltasColl = db.GetCollection<BsonDocument>("BTCdeltasTEST");
 
-        public static void StartDataUpdates()
+        private static IMongoCollection<BsonDocument> BTCpairsCollection;
+        private static IMongoCollection<BsonDocument> ETHpairsCollection;
+        private static IMongoCollection<BsonDocument> BNBpairsCollection;
+        private static IMongoCollection<BsonDocument> USDTpairsCollection;
+        private static List<BsonDocument> BTCdocuments;
+        private static List<BsonDocument> ETHdocuments;
+        private static List<BsonDocument> BNBdocuments;
+        private static List<BsonDocument> USDTdocuments;
+
+        public static async void StartDataUpdates()
         {
+            var exists = await CollectionExistsAsync("BTCmarketsTEST");
+            if (!exists)
+            {
+                var options = new CreateCollectionOptions() { StorageEngine = BsonDocument.Parse("{wiredTiger:{configString:'block_compressor=zlib'}}") };
+                db.CreateCollection("BTCdeltasTEST", options);
+            }
+            BTCpairsCollection = db.GetCollection<BsonDocument>("BTCmarkets");
+            ETHpairsCollection = db.GetCollection<BsonDocument>("ETHmarkets");
+            BNBpairsCollection = db.GetCollection<BsonDocument>("BNBmarkets");
+            USDTpairsCollection = db.GetCollection<BsonDocument>("USDTmarkets");
+
+            BTCdocuments = new List<BsonDocument>();
+            ETHdocuments = new List<BsonDocument>();
+            BNBdocuments = new List<BsonDocument>();
+            USDTdocuments = new List<BsonDocument>();
+
+
             //Begin Dequeue Thread:
             var DequeueThread = new Thread(() => ProcessQueue());
             DequeueThread.IsBackground = true;
@@ -28,37 +53,18 @@ namespace BeyonceCapture
         }
 
         public static void ProcessQueue()
-        {
-            
-            //IMongoCollection<BsonDocument> ETHdeltasColl = db.GetCollection<BsonDocument>("BTCdeltas");
-            //IMongoCollection<BsonDocument> BNBdeltasColl = db.GetCollection<BsonDocument>("BTCdeltas");
-
-            InsertManyOptions options = new InsertManyOptions()
-            {               
-                IsOrdered = false
-            };
-
-            var documents = new List<BsonDocument>();
-
+        {           
+                        
             while (true)
             {
                 if (UpdateQueue.IsEmpty)
                 {
                     // no pending updates available pause
-                    if (UpdateQueue.Count == 0 && documents.Count > 20)
-                    {
-                        BTCdeltasColl.InsertManyAsync(documents, options).Wait();
-                        Console.WriteLine($"1Wrote docs: {documents.Count}");
-                        documents = new List<BsonDocument>();
-                    }
-
+                    SendAllData();
                     Thread.Sleep(100);
                     continue;
                 }
-
-
-
-
+            
                 bool tryDQ = false;
                 do
                 {
@@ -66,31 +72,84 @@ namespace BeyonceCapture
                     tryDQ = UpdateQueue.TryDequeue(out msg);
                     if (tryDQ)
                     {
-
+                        //TODO: SORT MESSAGES, 
+                        //      ID = TIMESTAMP
+                        //      TRADE AND DEPTH SCHEMA?
+                       
 
                         var bsonMsg = BsonSerializer.Deserialize<BsonDocument>(msg);
-                        documents.Add(bsonMsg);
+                        //BTCdocuments.Add(bsonMsg);
 
 
                     }
                 } while (!tryDQ);
 
 
-                if (documents.Count > 80)
+                if (BTCdocuments.Count > 80 || ETHdocuments.Count > 80 || BNBdocuments.Count > 80 || USDTdocuments.Count > 80)
                 {
-                    BTCdeltasColl.InsertManyAsync(documents, options).Wait();
-                    Console.WriteLine($"2Wrote docs: {documents.Count}");
-                    documents = new List<BsonDocument>();
+                    SendAllData();
                 }
-
-
-
-
-
-
 
             }
         }
+
+
+        private static void SendAllData()
+        {
+            SendBTCdata();
+            SendETHdata();
+            SendBNBdata();
+            SendUSDTdata();
+        }
+
+
+        private static void SendBTCdata()
+        {
+            if (BTCdocuments.Count > 0)
+            {
+                BTCpairsCollection.InsertManyAsync(BTCdocuments, new InsertManyOptions() { IsOrdered = false }).Wait();
+                Console.WriteLine($"BTC docs: {BTCdocuments.Count}");
+                BTCdocuments = new List<BsonDocument>();
+            }
+        }
+        private static void SendETHdata()
+        {
+            if (ETHdocuments.Count > 0)
+            {
+                ETHpairsCollection.InsertManyAsync(ETHdocuments, new InsertManyOptions() { IsOrdered = false }).Wait();
+                Console.WriteLine($"ETH docs: {ETHdocuments.Count}");
+                ETHdocuments = new List<BsonDocument>();
+            }
+        }
+        private static void SendBNBdata()
+        {
+            if (BNBdocuments.Count > 0)
+            {
+                BNBpairsCollection.InsertManyAsync(BNBdocuments, new InsertManyOptions() { IsOrdered = false }).Wait();
+                Console.WriteLine($"BNB docs: {BNBdocuments.Count}");
+                BNBdocuments = new List<BsonDocument>();
+            }
+        }
+        private static void SendUSDTdata()
+        {
+            if (USDTdocuments.Count > 0)
+            {
+                USDTpairsCollection.InsertManyAsync(USDTdocuments, new InsertManyOptions() { IsOrdered = false }).Wait();
+                Console.WriteLine($"USDT docs: {USDTdocuments.Count}");
+                USDTdocuments = new List<BsonDocument>();
+            }
+        }
+
+
+        private static async Task<bool> CollectionExistsAsync(string collectionName)
+        {
+            var filter = new BsonDocument("name", collectionName);
+            //filter by collection name
+            var collections = await db.ListCollectionsAsync(new ListCollectionsOptions { Filter = filter });
+            //check for existence
+            return await collections.AnyAsync();
+        }
+
     }
 
     
--- BeyonceCapture/Program.cs
+++ BeyonceCapture/Program.cs
@@ -24,58 +24,40 @@ namespace BeyonceCapture
             Console.ReadLine();
             Environment.Exit(0);
         }
-
+        
 
         static async Task MainAsync()
         {
            
-
             var infos = await BeyonceREST.Get24hInfo();
-
-            var BTCdeltas = from info in infos
-                            where info.symbol.Substring(info.symbol.Length - 3, 3) == "BTC"
-                            orderby info.quoteVolume descending
-                            select info.symbol;
-
-            var ETHdeltas = from info in infos
-                            where info.symbol.Substring(info.symbol.Length - 3, 3) == "ETH"
-                            orderby info.quoteVolume descending
-                            select info.symbol;
-
-            //var BNBdeltas = from info in infos
-            //                where info.symbol.Substring(info.symbol.Length - 3, 3) == "BNB"
-            //                orderby info.quoteVolume descending
-            //                select info.symbol;
-
-            //var USDTdeltas = from info in infos
-            //                 where info.symbol.Substring(info.symbol.Length - 4, 4) == "USDT"
-            //                 orderby info.quoteVolume descending
-            //                 select info.symbol;
-
-
+            var BTCpairs = GetPairingsByVol(infos, "BTC");
+            var ETHpairs = GetPairingsByVol(infos, "ETH");
+            //var BNBpairs = GetPairingsByVol(infos, "BNB");
+            //var USDTpairs = GetPairingsByVol(infos, "USDT");
+            
             BeyonceDATA.StartDataUpdates();
-            MarketSocket socket = new MarketSocket(BTCdeltas.Take(20).ToList());
+            MarketSocket socket = new MarketSocket(BTCpairs.Take(20).ToList());
             socket.Connect();
-            MarketSocket socket2 = new MarketSocket(BTCdeltas.Skip(20).Take(20).ToList());
+            MarketSocket socket2 = new MarketSocket(BTCpairs.Skip(20).Take(20).ToList());
             socket2.Connect();
-            MarketSocket socket3 = new MarketSocket(BTCdeltas.Skip(40).Take(20).ToList());
+            MarketSocket socket3 = new MarketSocket(BTCpairs.Skip(40).Take(20).ToList());
             socket3.Connect();
-            MarketSocket socket4 = new MarketSocket(BTCdeltas.Skip(60).Take(20).ToList());
+            MarketSocket socket4 = new MarketSocket(BTCpairs.Skip(60).Take(20).ToList());
             socket4.Connect();
-            MarketSocket socket5 = new MarketSocket(BTCdeltas.Skip(80).Take(20).ToList());
+            MarketSocket socket5 = new MarketSocket(BTCpairs.Skip(80).Take(20).ToList());
             socket5.Connect();
 
 
-            MarketSocket socketE = new MarketSocket(ETHdeltas.Take(20).ToList());
+            MarketSocket socketE = new MarketSocket(ETHpairs.Take(20).ToList());
             socketE.Connect();
-            MarketSocket socket2E = new MarketSocket(ETHdeltas.Skip(20).Take(20).ToList());
+            MarketSocket socket2E = new MarketSocket(ETHpairs.Skip(20).Take(20).ToList());
             socket2E.Connect();
-            MarketSocket socket3E = new MarketSocket(ETHdeltas.Skip(40).Take(20).ToList());
+            MarketSocket socket3E = new MarketSocket(ETHpairs.Skip(40).Take(20).ToList());
             socket3E.Connect();
-            MarketSocket socket4E = new MarketSocket(ETHdeltas.Skip(60).Take(20).ToList());
+            MarketSocket socket4E = new MarketSocket(ETHpairs.Skip(60).Take(20).ToList());
             socket4E.Connect();
 
-            var R = ETHdeltas.Skip(80).Take(20).ToList();
+            var R = ETHpairs.Skip(80).Take(20).ToList();
 
             if (R.Count > 0)
             {
@@ -89,7 +71,13 @@ namespace BeyonceCapture
 
         }
 
-
+        private static List<string> GetPairingsByVol(List<Info24h> infos, string quoteSymbol)
+        {            
+            return (from info in infos
+                    where info.symbol.Substring(info.symbol.Length - quoteSymbol.Length, quoteSymbol.Length) == quoteSymbol
+                    orderby info.quoteVolume descending
+                    select info.symbol).ToList();
+        }
 
 
     }
